541c6fd2ff30c47a5cebafb6bac877915417f6ac
Denis Iliukovich <dediluk@gmail.com>
2021-08-11 14:36:43 +0300

    added signup page

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signupUser(request):
+    if request.method == "GET":
+        return render(request, 'todo/signupUser.html', {'form': UserCreationForm})
+    else:
+        # User.objects.create_user()
+        pass
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #authy
+    path('signup/', views.signupUser, name='signupUser')
 ]
